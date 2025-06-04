63e7171267d227fb340a611d8d2abbd5bcb6a947
satrianugrahasatt <nugrahasatria05@gmail.com>
2025-06-05 03:12:08 +0700

    update trustproxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Middleware;
 
-use Fideloper\Proxy\TrustProxies as Middleware;
+// Jika Anda menggunakan Laravel versi < 9, baris ini benar.
+// use Fideloper\Proxy\TrustProxies as Middleware;
+// Jika Anda menggunakan Laravel versi >= 9, seharusnya:
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+
 use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
@@ -12,12 +16,18 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies;
+    // Modifikasi di sini:
+    protected $proxies = '*'; // Ini mengizinkan semua proxy, aman untuk Cloud Run
 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_AWS_ELB;
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO | // Pastikan ini ada
+        Request::HEADER_X_FORWARDED_AWS_ELB; // Untuk Laravel < 9. Di Laravel >= 9, ini diganti Request::HEADER_X_FORWARDED_PREFIX
 }
