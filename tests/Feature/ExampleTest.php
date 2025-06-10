094bf528947a18ebbe5177ba77159fe8b57d469a
satrianugrahasatt <nugrahasatria05@gmail.com>
2025-06-11 03:52:28 +0700

    balikin h3h3 tamba error

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -11,21 +11,11 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    // public function test_basic_test()
-    // {
-    //     $response = $this->get('/');
-
-    //     $response->assertStatus(200);
-    // }
-
     public function test_basic_test()
     {
-        $adminRole = \App\Models\Role::create(['name' => 'Administrator']);
-        $menu = \App\Models\Menu::create(['name' => 'Home', 'url' => '/']);
-        \App\Models\Access::create(['role_id' => $adminRole->id, 'menu_id' => $menu->id]);
-        \App\Models\Admin::create(['role_id' => $adminRole->id]);
-
         $response = $this->get('/');
+
         $response->assertStatus(200);
     }
+
 }
