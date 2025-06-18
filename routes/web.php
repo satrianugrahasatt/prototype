299e949c86701c8e35a47aeb7451c0d54762f9e2
satrianugrahasatt <nugrahasatria05@gmail.com>
2025-06-18 17:29:43 +0700

    tambah leave req

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\AnnouncementsController;
+use App\Http\Controllers\UsersController;
 use App\Http\Controllers\AttendancesController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DepartmentsController;
+use App\Http\Controllers\EmployeeLeaveRequestsController;
+use App\Http\Controllers\EmployeeLeavesController;
 use App\Http\Controllers\EmployeesController;
+use App\Http\Controllers\EmployeeScoresController;
 use App\Http\Controllers\LogsController;
 use App\Http\Controllers\PositionsController;
 use App\Http\Controllers\ProfilesController;
@@ -12,7 +16,6 @@ use App\Http\Controllers\RecruitmentCandidatesController;
 use App\Http\Controllers\RecruitmentsController;
 use App\Http\Controllers\RolesController;
 use App\Http\Controllers\ScoreCategoriesController;
-use App\Http\Controllers\UsersController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -31,7 +34,7 @@ use Illuminate\Support\Facades\Route;
 Auth::routes([
     'register' => false,
     'verify' => false,
-    'reset' => false,
+    'reset' => false
 ]);
 
 Route::get('/', [WelcomeController::class, 'index'])->name('welcome')->middleware('guest');
@@ -43,7 +46,7 @@ Route::post('/recruitment-candidates', [RecruitmentCandidatesController::class, 
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::middleware('check.access')->group(function () {
+Route::middleware('check.access')->group(function() {
     Route::get('/employees-data', [EmployeesController::class, 'index'])->name('employees-data');
     Route::get('/employees-data/create', [EmployeesController::class, 'create'])->name('employees-data.create');
     Route::get('/employees-data/print', [EmployeesController::class, 'print'])->name('employees-data.print');
@@ -68,6 +71,22 @@ Route::middleware('check.access')->group(function () {
     Route::post('/positions-data', [PositionsController::class, 'store'])->name('positions-data.store');
     Route::put('/positions-data/{position}', [PositionsController::class, 'update'])->name('positions-data.update');
     Route::delete('/positions-data/{position}', [PositionsController::class, 'destroy'])->name('positions-data.destroy');
+    Route::get('/employees-performance-score', [EmployeeScoresController::class, 'index'])->name('employees-performance-score');
+    Route::get('/employees-performance-score/create', [EmployeeScoresController::class, 'create'])->name('employees-performance-score.create');
+    Route::get('/employees-performance-score/print', [EmployeeScoresController::class, 'print'])->name('employees-performance-score.print');
+    Route::get('/employees-performance-score/{employeeScore}', [EmployeeScoresController::class, 'show'])->name('employees-performance-score.show');
+    Route::get('/employees-performance-score/{employeeScore}/edit', [EmployeeScoresController::class, 'edit'])->name('employees-performance-score.edit');
+    Route::post('/employees-performance-score', [EmployeeScoresController::class, 'store'])->name('employees-performance-score.store');
+    Route::put('/employees-performance-score/{employeeScore}', [EmployeeScoresController::class, 'update'])->name('employees-performance-score.update');
+    Route::delete('/employees-performance-score/{employeeScore}', [EmployeeScoresController::class, 'destroy'])->name('employees-performance-score.destroy');
+    Route::get('/employees-leave-request', [EmployeeLeaveRequestsController::class, 'index'])->name('employees-leave-request');
+    Route::get('/employees-leave-request/create', [EmployeeLeaveRequestsController::class, 'create'])->name('employees-leave-request.create');
+    Route::get('/employees-leave-request/print', [EmployeeLeaveRequestsController::class, 'print'])->name('employees-leave-request.print');
+    Route::get('/employees-leave-request/{employeeLeaveRequest}', [EmployeeLeaveRequestsController::class, 'show'])->name('employees-leave-request.show');
+    Route::get('/employees-leave-request/{employeeLeaveRequest}/edit', [EmployeeLeaveRequestsController::class, 'edit'])->name('employees-leave-request.edit');
+    Route::post('/employees-leave-request', [EmployeeLeaveRequestsController::class, 'store'])->name('employees-leave-request.store');
+    Route::put('/employees-leave-request/{employeeLeaveRequest}', [EmployeeLeaveRequestsController::class, 'update'])->name('employees-leave-request.update');
+    Route::delete('/employees-leave-request/{employeeLeaveRequest}', [EmployeeLeaveRequestsController::class, 'destroy'])->name('employees-leave-request.destroy');
     Route::get('/attendances', [AttendancesController::class, 'index'])->name('attendances');
     Route::get('/attendances/print', [AttendancesController::class, 'print'])->name('attendances.print');
     Route::post('/attendances', [AttendancesController::class, 'store'])->name('attendances.store');
@@ -80,6 +99,14 @@ Route::middleware('check.access')->group(function () {
     Route::post('/announcements', [AnnouncementsController::class, 'store'])->name('announcements.store');
     Route::put('/announcements/{announcement}', [AnnouncementsController::class, 'update'])->name('announcements.update');
     Route::delete('/announcements/{announcement}', [AnnouncementsController::class, 'destroy'])->name('announcements.destroy');
+    Route::get('/recruitments', [RecruitmentsController::class, 'index'])->name('recruitments');
+    Route::get('/recruitments/create', [RecruitmentsController::class, 'create'])->name('recruitments.create');
+    Route::get('/recruitments/print', [RecruitmentsController::class, 'print'])->name('recruitments.print');
+    Route::get('/recruitments/{recruitment}', [RecruitmentsController::class, 'show'])->name('recruitments.show');
+    Route::get('/recruitments/{recruitment}/edit', [RecruitmentsController::class, 'edit'])->name('recruitments.edit');
+    Route::post('/recruitments', [RecruitmentsController::class, 'store'])->name('recruitments.store');
+    Route::put('/recruitments/{recruitment}', [RecruitmentsController::class, 'update'])->name('recruitments.update');
+    Route::delete('/recruitments/{recruitment}', [RecruitmentsController::class, 'destroy'])->name('recruitments.destroy');
     Route::get('/score-categories', [ScoreCategoriesController::class, 'index'])->name('score-categories');
     Route::get('/score-categories/create', [ScoreCategoriesController::class, 'create'])->name('score-categories.create');
     Route::get('/score-categories/print', [ScoreCategoriesController::class, 'print'])->name('score-categories.print');
@@ -101,23 +128,4 @@ Route::middleware('check.access')->group(function () {
     Route::delete('/roles/{role}', [RolesController::class, 'destroy'])->name('roles.destroy');
     Route::get('/profile', [ProfilesController::class, 'index'])->name('profile');
     Route::put('/profile/{user}', [ProfilesController::class, 'update'])->name('profile.update');
-    Route::get('/recruitments', [RecruitmentsController::class, 'index'])->name('recruitments');
-    Route::get('/recruitments/create', [RecruitmentsController::class, 'create'])->name('recruitments.create');
-    Route::get('/recruitments/print', [RecruitmentsController::class, 'print'])->name('recruitments.print');
-    Route::get('/recruitments/{recruitment}', [RecruitmentsController::class, 'show'])->name('recruitments.show');
-    Route::get('/recruitments/{recruitment}/edit', [RecruitmentsController::class, 'edit'])->name('recruitments.edit');
-    Route::post('/recruitments', [RecruitmentsController::class, 'store'])->name('recruitments.store');
-    Route::put('/recruitments/{recruitment}', [RecruitmentsController::class, 'update'])->name('recruitments.update');
-    Route::delete('/recruitments/{recruitment}', [RecruitmentsController::class, 'destroy'])->name('recruitments.destroy');
-<<<<<<< feature
-    Route::get('/employees-leave-request', [EmployeeLeaveRequestsController::class, 'index'])->name('employees-leave-request');
-    Route::get('/employees-leave-request/create', [EmployeeLeaveRequestsController::class, 'create'])->name('employees-leave-request.create');
-    Route::get('/employees-leave-request/print', [EmployeeLeaveRequestsController::class, 'print'])->name('employees-leave-request.print');
-    Route::get('/employees-leave-request/{employeeLeaveRequest}', [EmployeeLeaveRequestsController::class, 'show'])->name('employees-leave-request.show');
-    Route::get('/employees-leave-request/{employeeLeaveRequest}/edit', [EmployeeLeaveRequestsController::class, 'edit'])->name('employees-leave-request.edit');
-    Route::post('/employees-leave-request', [EmployeeLeaveRequestsController::class, 'store'])->name('employees-leave-request.store');
-    Route::put('/employees-leave-request/{employeeLeaveRequest}', [EmployeeLeaveRequestsController::class, 'update'])->name('employees-leave-request.update');
-    Route::delete('/employees-leave-request/{employeeLeaveRequest}', [EmployeeLeaveRequestsController::class, 'destroy'])->name('employees-leave-request.destroy');
-=======
->>>>>>> main
 });
