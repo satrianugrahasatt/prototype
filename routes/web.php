e3bada665d0a31442d8f753199ce4bcae0bd988a
satrianugrahasatt <nugrahasatria05@gmail.com>
2025-06-18 17:35:42 +0700

    benerin routing

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AttendancesController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DepartmentsController;
 use App\Http\Controllers\EmployeeLeaveRequestsController;
+use App\Http\Controllers\EmployeeLeavesController;
 use App\Http\Controllers\EmployeesController;
 use App\Http\Controllers\EmployeeScoresController;
 use App\Http\Controllers\LogsController;
